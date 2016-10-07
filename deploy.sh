6bcc06fbea381f6fb3f5beb69dddf18cbb683796
Joseph Iussa <jiussa@internode.on.net>
2016-10-07 16:22:56 +1030

    Added automatic deploy to github pages on push.

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -o errexit -o nounset
+
+if [ "$TRAVIS_BRANCH" != "master" ]
+then
+  echo "This commit was made against the $TRAVIS_BRANCH and not the master! No deploy!"
+  exit 0
+fi
+
+msg=$(git show -s --oneline HEAD)
+
+cd app
+
+git init
+git config user.name "Joseph Iussa"
+git config user.email "jiussa@internode.on.net"
+
+git remote add upstream "https://$GH_TOKEN@github.com/joseph-iussa/tour-planner.git"
+git fetch upstream && git reset upstream/gh-pages
+
+touch .
+
+git add -A .
+git commit -m "Rebuild from ${msg}."
+git push -q upstream HEAD:gh-pages
