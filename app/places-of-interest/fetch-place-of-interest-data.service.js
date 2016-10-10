15701c871fd4754cd5ac607a6f83e3a0980a1bf2
Joseph Iussa <jiussa@internode.on.net>
2016-10-11 00:25:27 +1030

    fetchPlaceOfInterestData service now wraps response in a native Angular promise. Added basic error handling to fetchPlaceOfInterestData.

--- app/places-of-interest/fetch-place-of-interest-data.service.js
+++ app/places-of-interest/fetch-place-of-interest-data.service.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('placesOfInterest').
-service('fetchPlaceOfInterestData', function (BING_API_KEY) {
+service('fetchPlaceOfInterestData', function ($q, BING_API_KEY) {
     return function(lat, lng, radius) {
         var url = 'https://spatial.virtualearth.net/REST/v1/data/f22876ec257b474b82fe2ffcb8393150/NavteqNA/NavteqPOIs';
         var params = {
@@ -11,18 +11,18 @@ service('fetchPlaceOfInterestData', function (BING_API_KEY) {
             'key': BING_API_KEY
         };
 
-        // Using jQuery here because couldn't get Angular's $http.jsonp to work with bing's API.
-        var deferred = $.Deferred();
-
-        $.ajax({
-            url: url,
-            jsonp: "jsonp",
-            dataType: "jsonp",
-            data: params,
-        }).done(function (response, textStatus, jqXHR) {
-            deferred.resolve(response.d.results);
+        return $q(function (resolve, reject) {
+            // Using jQuery here because couldn't get Angular's $http.jsonp nor $resource to work with bing's API.
+            $.ajax({
+                url: url,
+                jsonp: "jsonp",
+                dataType: "jsonp",
+                data: params,
+            }).done(function (response) {
+                resolve(response.d.results);
+            }).fail(function (jqXHR, textStatus, errorThrown) {
+                reject(textStatus);
+            });
         });
-
-        return deferred;
     };
 });
